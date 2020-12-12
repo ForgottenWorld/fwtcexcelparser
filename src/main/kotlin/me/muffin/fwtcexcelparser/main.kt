54c040c72921fe82618a13b6c8f052eb88841035
giacomo <giacomozama@hotmail.it>
2020-12-12 17:57:22 +0100

    wrapping

--- src/main/kotlin/me/muffin/fwtcexcelparser/main.kt
+++ src/main/kotlin/me/muffin/fwtcexcelparser/main.kt
@@ -10,6 +10,14 @@ import java.io.FileInputStream
 
 @Serializable data class Config(val Cards: Map<String, Map<String, Card>>)
 @Serializable data class Card(val Series: String, val Type: String, @SerialName("Has-Shiny-Version") val HasShiny: Boolean, val Info: String)
-fun main(args: Array<String>) = WorkbookFactory.create(FileInputStream(args[0])).getSheetAt(0).drop(1).map { r -> r.take(6).map { "$it" } }
-    .groupBy { it[0] }.mapValues { (_,v) -> v.associate { it[1] to Card(it[2], it[3], it[4].toBoolean(), it.getOrElse(5) { "" }) } }
-    .let(::Config).let(Yaml()::encodeToString).replace(""""(.+)":""".toRegex()) { "${it.groupValues[1]}:" }.let(File("output.yml")::writeText)
+fun main(args: Array<String>) = WorkbookFactory
+    .create(FileInputStream(args[0]))
+    .getSheetAt(0)
+    .drop(1)
+    .map { r -> r.take(6).map { "$it" } }
+    .groupBy { it[0] }
+    .mapValues { (_,v) -> v.associate { it[1] to Card(it[2], it[3], it[4].toBoolean(), it.getOrElse(5) { "" }) } }
+    .let(::Config)
+    .let(Yaml()::encodeToString)
+    .replace(""""(.+)":""".toRegex()) { "${it.groupValues[1]}:" }
+    .let(File("output.yml")::writeText)
