3cbd204d14f976eddc8ef53109a546c5b24c644f
giacomo <giacomozama@hotmail.it>
2020-12-12 18:10:33 +0100

    whitespace replacement

--- src/main/kotlin/me/muffin/fwtcexcelparser/main.kt
+++ src/main/kotlin/me/muffin/fwtcexcelparser/main.kt
@@ -10,14 +10,15 @@ import java.io.FileInputStream
 
 @Serializable data class Config(val Cards: Map<String, Map<String, Card>>)
 @Serializable data class Card(val Series: String, val Type: String, @SerialName("Has-Shiny-Version") val HasShiny: Boolean, val Info: String)
+val String.noWhitespace get() = replace(" ", "_")
 
 fun main(args: Array<String>) = WorkbookFactory
     .create(FileInputStream(args[0]))
     .getSheetAt(0)
     .drop(1)
     .map { r -> r.take(6).map { "$it" } }
-    .groupBy { it[0] }
-    .mapValues { (_,v) -> v.associate { it[1] to Card(it[2], it[3], it[4].toBoolean(), it.getOrElse(5) { "" }) } }
+    .groupBy { it[0].noWhitespace }
+    .mapValues { (_,v) -> v.associate { it[1].noWhitespace to Card(it[2], it[3].noWhitespace, it[4].toBoolean(), it.getOrElse(5) { "" }) } }
     .let(::Config)
     .let(Yaml()::encodeToString)
     .replace(""""(.+)":""".toRegex()) { "${it.groupValues[1]}:" }
